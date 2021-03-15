101c5348b6279ffc28e81a650993b360eb598eaf
Huakun Shen <huakun.shen@huakunshen.com>
2021-03-15 04:53:03 -0400

    Finished converting Login and Signup page to material ui + dark theme

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Signup from "./Signup";
-import { Container } from "react-bootstrap";
+// import { Container } from "react-bootstrap";
+import Container from "@material-ui/core/Container";
 import { AuthProvider } from "../contexts/AuthContext";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Dashboard from "./Dashboard";
@@ -8,16 +9,46 @@ import Login from "./Login";
 import PrivateRoute from "./PrivateRoute";
 import ForgotPassword from "./ForgotPassword";
 import UpdateProfile from "./UpdateProfile";
+import {
+  createMuiTheme,
+  makeStyles,
+  ThemeProvider,
+} from "@material-ui/core/styles";
+import { Paper } from "@material-ui/core";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+// const theme = createMuiTheme({
+//   palette: {
+//     type: "dark",
+//   },
+// });
 
 const App = () => {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: "dark",
+          // type: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
+
+  const useStyles = makeStyles({
+    root: {
+      backgroundColor: theme.palette.background.default,
+      minHeight: "100vh",
+    },
+  });
+  const classes = useStyles();
+
   return (
-    <Container
-      className='d-flex align-items-center justify-content-center'
-      style={{ minHeight: "100vh" }}
-    >
-      <div className='w-100' style={{ maxWidth: "400px" }}>
-        <Router>
-          <AuthProvider>
+    <Router>
+      <AuthProvider>
+        <ThemeProvider theme={theme}>
+          <Paper className={classes.root} square>
             <Switch>
               <PrivateRoute exact path='/' component={Dashboard} />
               <PrivateRoute path='/update-profile' component={UpdateProfile} />
@@ -25,10 +56,10 @@ const App = () => {
               <Route path='/login' component={Login} />
               <Route path='/forgot-password' component={ForgotPassword} />
             </Switch>
-          </AuthProvider>
-        </Router>
-      </div>
-    </Container>
+          </Paper>
+        </ThemeProvider>
+      </AuthProvider>
+    </Router>
   );
 };
 
--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,7 +1,24 @@
 import React, { useRef, useState } from "react";
-import { Form, Button, Card, Alert } from "react-bootstrap";
+import { Form } from "react-bootstrap";
 import { useAuth } from "../contexts/AuthContext";
 import { Link, useHistory } from "react-router-dom";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import { Container, Box, TextField } from "@material-ui/core";
+import Alert from "@material-ui/lab/Alert";
+
+const useStyles = makeStyles((theme) => ({
+  card: {
+    minWidth: 400,
+    backgroundColor: theme.palette.background.paper,
+    transform: "translate(0%,-50%)",
+    padding: "1em 2em 1em 2em",
+  },
+}));
 
 const Signup = () => {
   const emailRef = useRef();
@@ -11,6 +28,7 @@ const Signup = () => {
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const history = useHistory();
+  const classes = useStyles();
 
   async function handleSubmit(e) {
     e.preventDefault();
@@ -33,32 +51,67 @@ const Signup = () => {
 
   return (
     <>
-      <Card>
-        <Card.Body>
-          <h2 className='text-center mb-4'>Sign Up</h2>
-          {error && <Alert variant='danger'>{error}</Alert>}
-          <Form onSubmit={handleSubmit}>
-            <Form.Group id='email'>
-              <Form.Label>Email</Form.Label>
-              <Form.Control type='email' ref={emailRef} required />
-            </Form.Group>
-            <Form.Group id='password'>
-              <Form.Label>Password</Form.Label>
-              <Form.Control type='password' ref={passwordRef} required />
-            </Form.Group>
-            <Form.Group id='password-confirm'>
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type='password' ref={passwordConfirmRef} required />
-            </Form.Group>
-            <Button disabled={loading} className='w-100' type='submit'>
-              Sign Up
-            </Button>
-          </Form>
-        </Card.Body>
-      </Card>
-      <div className='w-100 text-center mt-2'>
-        Already have an account? <Link to='/login'>Log In</Link>
-      </div>
+      <Container>
+        <Box
+          display='flex'
+          justifyContent='center'
+          alignItems='center'
+          p={1}
+          height='100vh'
+          bgcolor='background.default'
+        >
+          <Card className={classes.card} color='secondary'>
+            <Form onSubmit={handleSubmit}>
+              <CardContent>
+                <Typography
+                  variant='h3'
+                  component='h2'
+                  style={{ marginBottom: "1em", textAlign: "center" }}
+                >
+                  Sign Up
+                </Typography>
+
+                {error && <Alert severity='error'>{error}</Alert>}
+                <div>
+                  <TextField
+                    fullWidth
+                    inputRef={emailRef}
+                    margin='normal'
+                    label='Email'
+                    type='email'
+                  />
+                </div>
+                <div>
+                  <TextField
+                    fullWidth
+                    margin='normal'
+                    inputRef={passwordRef}
+                    label='Password'
+                    type='password'
+                  />
+                </div>
+                <div>
+                  <TextField
+                    fullWidth
+                    margin='normal'
+                    inputRef={passwordConfirmRef}
+                    label='Confirm Password'
+                    type='password'
+                  />
+                </div>
+                <Button fullWidth type='submit' disabled={loading}>
+                  Sign Up
+                </Button>
+              </CardContent>
+            </Form>
+            <CardActions>
+              <Typography>
+                Already have an account? <Link to='/login'>Log In</Link>
+              </Typography>
+            </CardActions>
+          </Card>
+        </Box>
+      </Container>
     </>
   );
 };
