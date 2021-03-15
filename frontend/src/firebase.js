65184f21c434f530bc9a53aad4c5510e9e18cf37
Huakun Shen <huakun.shen@huakunshen.com>
2021-03-15 03:15:47 -0400

    Initialize frontend with a react firebase template (with email authentication).

---
+++ frontend/src/firebase.js
@@ -0,0 +1,17 @@
+import fb from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebase = fb;
+
+const app = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECTID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FIREBASE_APPID,
+});
+
+export const auth = app.auth();
+export default app;
