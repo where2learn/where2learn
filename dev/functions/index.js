2c446b571cfed0bf69f8a2df7e59b62c58580aef
Jiatao Xiang <jiatao.xiang@gmail.com>
2021-03-19 16:27:15 -0400

    add callable version

--- dev/functions/index.js
+++ dev/functions/index.js
@@ -14,7 +14,7 @@ admin.initializeApp();
 
 // without authentication version
 exports.increaseTagCount = functions.https.onRequest((req, res) => {
-  const value = req.query.value;
+  const value = req.query.value;  
   let documentRef = admin.firestore().doc('tags/' + value);
   documentRef.update(
     'count', firestore.FieldValue.increment(1)
@@ -23,4 +23,31 @@ exports.increaseTagCount = functions.https.onRequest((req, res) => {
   }).then(doc => {
     res.json({result: `${doc.get('value')} has current count ${doc.get('count')}`});
   })
+})
+
+exports.increaseTagCount = functions.https.onCall((data, context) => {
+  // context containing 
+  if (!context.auth) {
+    // Throwing an HttpsError so that the client gets the error details.
+    throw new functions.https.HttpsError('failed-precondition', 'The function must be called ' +
+        'while authenticated.');
+  }
+  const value = data.value;
+  // Authentication / user information is automatically added to the request.
+  // const uid = context.auth.uid;
+  const name = context.auth.token.name || null;
+
+  let documentRef = admin.firestore().doc('tags/' + value);
+  return documentRef.update(
+    'count', firestore.FieldValue.increment(1)
+  ).then(() => {
+    return documentRef.get(); 
+  }).then(doc => {
+    return {text: `Hi ${name}, ${doc.get('value')} has current count ${doc.get('count')}`}
+  })
+})
+
+// upload image
+exports.upload_image = functions.https.onRequest((req, res) => {
+
 })
