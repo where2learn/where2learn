810dda83948b24dd9332f996b6084e16abfcdc35
Huakun Shen <huakun.shen@huakunshen.com>
2021-03-23 03:29:17 -0400

    Minor update to Main.jsx to hide tags section when there is no tag at all

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,19 +1,19 @@
-import React, { useEffect, useState, useRef } from "react";
-import NavDrawer from "../components/NavDrawer";
-import { getModules } from "../firebase";
-import { makeStyles } from "@material-ui/core/styles";
-import Grid from "@material-ui/core/Grid";
-import ModuleList from "../components/ModuleList";
-import Paper from "@material-ui/core/Paper";
-import Chip from "@material-ui/core/Chip";
-import TextField from "@material-ui/core/TextField";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import AccountTreeIcon from "@material-ui/icons/AccountTree";
-import MenuBookIcon from "@material-ui/icons/MenuBook";
-import "../style/Main.scss";
+import React, { useEffect, useState, useRef } from 'react';
+import NavDrawer from '../components/NavDrawer';
+import { getModules } from '../firebase';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import ModuleList from '../components/ModuleList';
+import Paper from '@material-ui/core/Paper';
+import Chip from '@material-ui/core/Chip';
+import TextField from '@material-ui/core/TextField';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import AccountTreeIcon from '@material-ui/icons/AccountTree';
+import MenuBookIcon from '@material-ui/icons/MenuBook';
+import '../style/Main.scss';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -21,7 +21,7 @@ const useStyles = makeStyles((theme) => ({
   },
   tagPaper: {
     padding: theme.spacing(1),
-    textAlign: "center",
+    textAlign: 'center',
     color: theme.palette.text.secondary,
     margin: theme.spacing(1),
   },
@@ -35,12 +35,17 @@ const useStyles = makeStyles((theme) => ({
   chip: {
     margin: theme.spacing(0.5),
   },
+  menuBar: {
+    position: 'sticky',
+    top: '20px',
+    marginTop: '1rem',
+  },
 }));
 
 const Main = () => {
   const [modules, setModules] = useState([]);
   const [tags, setTags] = useState([]);
-  const [page, setPage] = useState("modules");
+  const [page, setPage] = useState('modules');
   const tagInputRef = useRef();
   const keywordInputRef = useRef();
   const classes = useStyles();
@@ -50,7 +55,7 @@ const Main = () => {
   };
 
   useEffect(() => {
-    setTags(["react", "vue"]);
+    setTags(['react', 'vue']);
     initAllModules();
     console.log(tagInputRef.current);
     tagInputRef.current.onChange = (e) => {
@@ -66,13 +71,13 @@ const Main = () => {
   const handleTagSubmit = (e) => {
     e.preventDefault();
     setTags(Array.from(new Set([tagInputRef.current.value, ...tags])));
-    tagInputRef.current.value = "";
+    tagInputRef.current.value = '';
   };
 
   const getSubPage = () => {
-    if (page === "modules") {
+    if (page === 'modules') {
       return <ModuleList modules={modules} />;
-    } else if (page === "roadmaps") {
+    } else if (page === 'roadmaps') {
       return <ModuleList modules={modules} />;
     } else {
       return null;
@@ -84,19 +89,21 @@ const Main = () => {
       <div className='main-page'>
         <Grid container spacing={3}>
           <Grid item xs={12} sm={12} md={3} lg={3} xl={2}>
-            <div className='sticky-top'>
-              <Paper className={classes.tagPaper}>
-                {tags.map((tag, index) => {
-                  return (
-                    <Chip
-                      key={index}
-                      label={tag}
-                      className={classes.chip}
-                      onDelete={handleDeleteTagChip(tag)}
-                    />
-                  );
-                })}
-              </Paper>
+            <div className={classes.menuBar}>
+              {tags && tags.length !== 0 && (
+                <Paper className={classes.tagPaper}>
+                  {tags.map((tag, index) => {
+                    return (
+                      <Chip
+                        key={index}
+                        label={tag}
+                        className={classes.chip}
+                        onDelete={handleDeleteTagChip(tag)}
+                      />
+                    );
+                  })}
+                </Paper>
+              )}
               <Paper className={classes.searchPaper}>
                 <form onSubmit={handleTagSubmit}>
                   <TextField
@@ -119,13 +126,13 @@ const Main = () => {
               </Paper>
               <Paper className={classes.menuPaper}>
                 <List component='nav'>
-                  <ListItem button onClick={() => setPage("modules")}>
+                  <ListItem button onClick={() => setPage('modules')}>
                     <ListItemIcon>
                       <MenuBookIcon />
                     </ListItemIcon>
                     <ListItemText primary='Modules' />
                   </ListItem>
-                  <ListItem button onClick={() => setPage("roadmaps")}>
+                  <ListItem button onClick={() => setPage('roadmaps')}>
                     <ListItemIcon>
                       <AccountTreeIcon />
                     </ListItemIcon>
