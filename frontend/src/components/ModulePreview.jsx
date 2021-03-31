4304d39d8eb944e82a6a3020dfeb26fafd8a805b
Huakun Shen <huakun.shen@huakunshen.com>
2021-03-31 01:40:46 -0400

    ModulePreview show all tags on hover done

--- frontend/src/components/ModulePreview.jsx
+++ frontend/src/components/ModulePreview.jsx
@@ -9,9 +9,13 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 import IconButton from '@material-ui/core/IconButton';
 import Badge from '@material-ui/core/Badge';
 import StarIcon from '@material-ui/icons/Star';
+import Popper from '@material-ui/core/Popper';
+import Paper from '@material-ui/core/Paper';
 import CardHeader from '@material-ui/core/CardHeader';
 import Divider from '@material-ui/core/Divider';
+import Button from '@material-ui/core/Button';
 import Chip from '@material-ui/core/Chip';
+import Fade from '@material-ui/core/Fade';
 import { Link } from 'react-router-dom';
 
 import { convertTagsObj2Array } from '../firestore_data';
@@ -42,6 +46,21 @@ const useStyles = makeStyles((theme) => ({
       color: theme.palette.text.secondary,
     },
   },
+  tagsPopper: {
+    backgroundColor: theme.palette.bg.l5,
+    maxWidth: 500,
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    padding: theme.spacing(2),
+    '& > *': {
+      margin: theme.spacing(0.5),
+    },
+  },
+  tagChips: {
+    backgroundColor: theme.palette.bg.l7,
+    color: theme.palette.bg.l1,
+  },
   tags: {
     marginTop: -10,
     display: 'flex',
@@ -57,11 +76,23 @@ const ModulePreview = (props) => {
   const classes = useStyles();
   const [tags, setTags] = useState([]);
 
+  const [tagsPopper, setTagsPopper] = React.useState(null);
+  const [open, setOpen] = React.useState(false);
+
   useEffect(() => {
     if (props.tags) {
       setTags(convertTagsObj2Array(props.tags));
     }
   }, [props.tags]);
+
+  const tagsOnMouseOver = (e) => {
+    setTagsPopper(e.currentTarget);
+    setOpen(true);
+  };
+
+  const tagsOnMouseOut = (e) => {
+    setOpen(false);
+  };
 
   return (
     <Card
@@ -88,11 +119,33 @@ const ModulePreview = (props) => {
       />
       <Divider />
       <CardContent className={classes.cardContent}>
-        <div className={classes.tags}>
+        <div
+          className={classes.tags}
+          onMouseOver={tagsOnMouseOver}
+          onMouseOut={tagsOnMouseOut}
+        >
           {tags.slice(0, previewMaxTags).map((tag, index) => {
             return <Chip size='small' key={index} label={tag} />;
           })}
         </div>
+        <Popper open={open} anchorEl={tagsPopper} placement='bottom' transition>
+          {({ TransitionProps }) => (
+            <Fade {...TransitionProps} timeout={350}>
+              <Paper className={classes.tagsPopper}>
+                {tags.map((tag, index) => {
+                  return (
+                    <Chip
+                      className={classes.tagChips}
+                      size='small'
+                      key={index}
+                      label={tag}
+                    />
+                  );
+                })}
+              </Paper>
+            </Fade>
+          )}
+        </Popper>
         <Typography
           // noWrap={true}
           variant='body2'
